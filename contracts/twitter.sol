28aafb4ee0a5207cb90d91a3a6def434531c8fe8
izzy-Ti <israelashenafi29@gmail.com>
2025-08-28 09:21:41 -0700

    Like and unlike functions

--- contracts/twitter.sol
+++ contracts/twitter.sol
@@ -4,33 +4,54 @@
 pragma solidity ^0.8.18;
 
 
-contract modify{
-        address public owner;
-        mapping(address => uint) public balance;
-        bool public paused;
+    contract twitter{
+
+    uint public TweetMaxLength = 200;
+
+        struct tweet{
+            uint id;
+            address author;
+            string content;
+            uint timeStamp;
+            uint likes;
+        }
+    function changeTweetLength (uint newTweetLength) public {
+        TweetMaxLength = newTweetLength;
+    }
+    address public owner;
     constructor(){
         owner = msg.sender;
-        balance[owner] = 1000;
-        paused = false;
     }
-    modifier onlyOwner{
-        require(msg.sender == owner, 'the user is not the owner');
-        _;
-    }
-    modifier isPaused{
-        require(paused == false, 'The system is paused');
-        _;
-    }
-    function pause() public onlyOwner {
-        paused = true;
-    }
-    function unpause() public onlyOwner {
-        paused = false;
-    }
-    function transaction(address to, uint amount) public isPaused {
-        require(balance[msg.sender] <= 500, 'Your balance is less than minimum');
 
-        balance[msg.sender] -= amount;
-        balance[to] += amount; 
+    modifier onlyOwner{
+        require(owner == msg.sender, 'The sender is not the owner');
+        _;
+    }
+
+    mapping(address => tweet[]) public tweets;
+    function createtweets (string memory _tweet) public onlyOwner {
+
+        require(bytes(_tweet).length <= 200, 'The tweet is so long');
+        tweet memory newTweet = tweet({
+            id: tweets[msg.sender].length,
+            author: msg.sender,
+            content: _tweet,
+            timeStamp: block.timestamp,
+            likes: 0
+        });
+        tweets[msg.sender].push(newTweet);
+    }
+    function likeTweet(address author, uint id) public {
+        require(tweets[author][id].id == id, 'The tweet doesnt exist');
+        tweets[author][id].likes ++ ;
+    }
+    function UnlikeTweet(address author, uint id) public {
+        tweets[author][id].likes-- ;
+    }
+    function gettweet(address _owner, uint _i) public view returns( tweet memory){
+        return tweets[_owner][_i];
+    }
+    function getAlltweets(address _owner) public view returns( tweet[] memory){
+        return tweets[_owner];
     }
 }
